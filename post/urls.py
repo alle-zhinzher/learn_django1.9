c1052beff1521215a890626ab2a30cbe2e1601ae
sasha2009 <37780107+sasha2009@users.noreply.github.com>
2019-01-27 18:17:40 +0200

    Make Pagination, social shared links, image upload fields, create autho slug

---
+++ post/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from post.views import (post_create,
+                        post_delete, post_detail,
+                        post_list, post_update)
+
+urlpatterns = [
+    path('', post_list, name='post-list'),
+    path('create/', post_create),
+    path('post/<str:slug>/edit/', post_update, name='post-detail'),
+    path('post/<str:slug>/', post_detail, name='post-detail'),
+    path('post/<str:slug>/delete/', post_delete, name='list-delete'),
+]
---
+++ post/views.py
@@ -0,0 +1,59 @@
+from urllib.parse import quote_plus
+from django.shortcuts import get_object_or_404, HttpResponseRedirect, redirect, render
+from django.contrib import messages
+from django.core.paginator import Paginator
+
+from post.forms import PostForm
+from post.models import Post
+
+
+def post_list(request):
+    queryset = Post.objects.all().order_by('-timestamp')
+    paginator = Paginator(queryset, 3)  # Show 25 contacts per page
+    page = request.GET.get('page')
+    post = paginator.get_page(page)
+    context = {
+        'object_list': post,
+        'title': 'list'
+    }
+    return render(request, 'posts_list.html', context=context)
+
+
+def post_create(request):
+    form = PostForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Successfully created")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+        messages.error(request, "Not successfully created")
+    return render(request, 'post_form.html', context={'form': form})
+
+
+def post_detail(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
+    share_string = quote_plus(instance.content)
+    return render(request, 'post_detail.html', context={'post': instance,
+                                                        'share_string': share_string,})
+
+
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, 'Post Updated')
+        return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+        messages.error(request, 'Post not updated')
+    return render(request, 'post_update.html', context={'post': instance,
+                                                        'form': form})
+
+
+def post_delete(request, slug=None):
+    instance = get_object_or_404(Post, id=slug)
+    instance.delete()
+    messages.success(request, 'Post Deleted')
+    return redirect('post-list')
