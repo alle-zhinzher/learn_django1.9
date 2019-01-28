f74ebc1144e3d379657cf6f6c644e4566a8e5a09
sasha2009 <37780107+sasha2009@users.noreply.github.com>
2019-01-28 19:27:36 +0200

    Added: Markdown, Django Pagedown, Responsive Image.

--- post/forms.py
+++ post/forms.py
@@ -1,9 +1,12 @@
 from django import forms
 
+from pagedown.widgets import PagedownWidget
 from post.models import Post
 
 
 class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=PagedownWidget)
+    publish = forms.DateField(widget=forms.SelectDateWidget)
     class Meta:
         model = Post
         fields = [
